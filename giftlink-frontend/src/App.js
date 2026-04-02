528dd6dc0e05d9802367ca216822b6c44873b9b0
Siphiwe444 <mdumsenizulu98@gmail.com>
2026-04-02 09:36:48 -0400

    DetailsPage added

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,23 +1,26 @@
+// App.js
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import your pages/components
 import MainPage from './components/MainPage/MainPage';
-import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import LoginPage from './components/LoginPage/LoginPage';
+import DetailsPage from './components/DetailsPage/DetailsPage'; // <-- Import DetailsPage
+
 function App() {
-  const navigate = useNavigate();
   return (
-		<>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-        </Routes>
-        </>
+    <Router>
+      <Routes>
+     
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
