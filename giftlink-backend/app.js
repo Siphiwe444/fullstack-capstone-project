91350f0eb4048914883012ebbc1a36a9e21c0c3a
Siphiwe444 <mdumsenizulu98@gmail.com>
2026-04-06 04:58:56 -0400

    Error fixed

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,19 +5,18 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
+const { loadData } = require("./util/import-mongo/index");
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
+connectToDatabase()
+    .then(() => {
+        pinoLogger.info('Connected to DB');
+    })
     .catch((e) => console.error('Failed to connect to DB', e));
-
 
 app.use(express.json());
 
@@ -41,8 +40,8 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-
-app.get("/",(req,res)=>{s
+// Root Route
+app.get("/", (req, res) => {
     res.send("Inside the server");
 });
 
